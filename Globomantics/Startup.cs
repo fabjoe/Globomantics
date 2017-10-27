7277c3c8c3bc024a44bf1f108b9e7c3ad24360a5
fabrizioc <fabrizioc@silversea.com>
2017-10-27 16:36:49 +0200

    Completed

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -8,11 +8,17 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Globomantics.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Globomantics
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +26,8 @@ namespace Globomantics
             services.AddMvc();
             services.AddSingleton<IConferenceService, ConferenceMemoryService>();
             services.AddSingleton<IProposalService, ProposalMemoryService>();
+
+            services.Configure<GlobomanticsOptions>(_configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Globomantics/ViewComponents/StatisticsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomantics.ViewComponents
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly IConferenceService _conferenceService;
+        public StatisticsViewComponent(IConferenceService conferenceService)
+        {
+            _conferenceService = conferenceService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _conferenceService.GetStatistics());
+        }
+    }
+}
